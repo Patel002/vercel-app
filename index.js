9cb5da80d37a128005ebbd9f95c27145ceb582f7
Vatsal <patelvatsal79715@gmail.com>
2024-12-02 18:44:53 +0530

    save changes

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
 
+
 dotenv.config({
   path: './.env'
+})
+
+
+app.listen(process.env.PORT , () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
 
-export default app; 
+
+
